7aa5f11e958686fd6434d8cd9b006af595b899ce
frinyo <rinyobot@gmail.com>
2024-05-02 07:03:05 +0700

    change factories

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -18,8 +18,9 @@ class OrderFactory extends Factory
             'user_id' => User::factory(),
             'total_price' => $this->faker->numberBetween(1, 9) . $this->faker->numerify('#000'),
             'address' => $this->faker->address(),
-            'status' => $this->faker->randomElement(['waiting', 'accepted', 'canceled', 'done']),
-            'payment_status' => 1,
+            // 'status' => $this->faker->randomElement(['waiting', 'accepted', 'canceled', 'done']),
+            'status' => 'done',
+            'payment_status' => 2,
             'created_at' => $this->faker->dateTimeBetween('-4 months', 'now'),
         ];
     }
